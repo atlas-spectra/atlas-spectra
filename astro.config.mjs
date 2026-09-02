a76cdb237ed2183babdd17f7952f3660c0464fc8
Harrison Powers <harrisonpowers@gmail.com>
2026-09-02 17:58:10 -0400

    fix(web): target project pages base path

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import react from "@astrojs/react";
 
 export default defineConfig({
   site: "https://atlas-spectra.github.io",
+  base: "/atlas-spectra",
   integrations: [react()],
   trailingSlash: "always",
 });
